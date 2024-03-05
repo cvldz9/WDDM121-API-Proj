b1b192ef4d2cefdd6f42aa00aa700b17d2f0c789
Catherine Valdez <catvaldez519@gmail.com>
2024-03-04 23:54:05 -0500

    first commit for  Developers page

--- src/pages/Developers.jsx
+++ src/pages/Developers.jsx
@@ -1,10 +1,15 @@
 import "./Developers.css";
+
 function Developers({ isDarkMode }) {
-	return (
-		<>
-			<div>Developers</div>
-		</>
-	);
+  return (
+    <div className="flex flex-col items-center">
+      <h3 className="text-center pb-8 pt-10 px-20 text-2xl self-center">Developers</h3>
+      <div className="flex justify-center pb-8 ">
+        <p className="sub-paragraph text-center">Explore this page to learn more about each member of our team. Discover their areas of expertise, passions, and contributions to our projects. Get to know the faces behind the code and see firsthand the dedication and passion that drive us forward. Join us on our journey as we collaborate, innovate, and create groundbreaking solutions together.</p>
+      </div>
+	  
+    </div>
+  );
 }
 
 export default Developers;
