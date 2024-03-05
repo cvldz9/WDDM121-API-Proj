1c4b7ae5709325fdaca2eee02c0dffcf662db18d
Catherine Valdez <catvaldez519@gmail.com>
2024-03-05 02:53:14 -0500

    first commit for contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,18 @@
 import "./Contact.css";
+
 function Contact({ isDarkMode }) {
-	return (
-		<>
-			<div>Contact</div>
-		</>
-	);
+  return (
+    <>
+      <div className="flex flex-col items-center">
+        <h3 className="text-center pb-8 pt-10 px-20 text-2xl self-center font-bold">Contact</h3>
+        <div className="flex justify-center pb-8">
+          <p className="sub-paragraph text-center">Reach out to us today and let's start a conversation. Whether you have questions, feedback, or simply want to say hello, we're here to listen and assist you. Get in touch and discover how we can collaborate to achieve your goals.</p>
+        </div>
+
+        
+      </div>
+    </>
+  );
 }
 
 export default Contact;
