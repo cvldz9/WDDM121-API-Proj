621e6ec752bb71530879eb50afa089633da84a2a
Patty Bactad <bactadpatty@gmail.com>
2024-03-07 01:26:14 -0500

    started updating about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,14 +3,12 @@ import GlobeImage from "../assets/globe.svg";
 function About({ isDarkMode }) {
 	return (
 		<>
-			<div className="about-wrap flex flex-col items-center justify-center gap-[4.375rem] p-10 w-full">
+			<div className="flex flex-col items-center">
 				<div className="flex flex-col">
-					<h3 className=" uppercase">About Us</h3>
+					<h1 className=" uppercase">About Us</h1>
 					<img className="w-[20rem]" src={GlobeImage} alt="" />
 				</div>
-				<div>Box 2</div>
-				<div>Box 3</div>
-				<div>Box 4</div>
+
 			</div>
 		</>
 	);
