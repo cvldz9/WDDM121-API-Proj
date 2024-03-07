7b3a64b755b6e283afca3f0abb0f6abccbd1eaa1
Patty Bactad <bactadpatty@gmail.com>
2024-03-07 01:57:15 -0500

    made the image larger and updated about description

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,20 +1,26 @@
 import "./About.css";
 import GlobeImage from "../assets/globe.svg";
+
 function About({ isDarkMode }) {
-	return (
-		<>
-			
-			<div className="flex flex-col items-center">
-                <h3 className="text-center pb-8 pt-10 px-20 text-2xl self-center font-bold">About</h3>
-                    <div className="flex justify-center pb-8">
-						<img className="w-[20rem]" src={GlobeImage} alt="" />
+  return (
+    <>
+      <div className="flex flex-col items-center">
+        <h3 className="text-center pb-8 pt-10 px-20 text-2xl self-center font-bold">About</h3>
+        <div className="flex justify-center items-center flex-col pb-8">
+          {/* Updated the image size and added utility classes for centering */}
+          <img className="w-[40rem] mb-4" src={GlobeImage} alt="" />
 
-							<p className="sub-paragraph text-center">Text</p>
-                    </div>
-            </div>
+          <p className="sub-paragraph text-center">
+            Our goal is to create an application that will provide users with personalized local information based on their geolocation.
 
-		</>
-	);
+            This platform will offer real-time weather updates and relevant local news to enhance the user experience and keep them informed about their surroundings.
+
+            It will also include the functionality to enable users to search the weather and news in any location at any given time. As well as the ability to filter news based on their references.
+          </p>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default About;
