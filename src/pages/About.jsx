de05c9782781f4242c1933d92d9ac2eb47e929d8
Patty Bactad <bactadpatty@gmail.com>
2024-03-07 01:48:37 -0500

    updated text spacing for the short description of about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,13 +3,16 @@ import GlobeImage from "../assets/globe.svg";
 function About({ isDarkMode }) {
 	return (
 		<>
+			
 			<div className="flex flex-col items-center">
-				<div className="flex flex-col">
-					<h1 className=" uppercase">About Us</h1>
-					<img className="w-[20rem]" src={GlobeImage} alt="" />
-				</div>
+                <h3 className="text-center pb-8 pt-10 px-20 text-2xl self-center font-bold">About</h3>
+                    <div className="flex justify-center pb-8">
+						<img className="w-[20rem]" src={GlobeImage} alt="" />
 
-			</div>
+							<p className="sub-paragraph text-center">Text</p>
+                    </div>
+            </div>
+
 		</>
 	);
 }
