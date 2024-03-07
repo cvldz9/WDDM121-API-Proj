9579069f8003e2463d149b0a8a15cb684625764c
Patty Bactad <bactadpatty@gmail.com>
2024-03-07 02:11:17 -0500

    debugged last line of featuredapi page

--- src/pages/FeaturedApi.jsx
+++ src/pages/FeaturedApi.jsx
@@ -4,17 +4,10 @@ function FeaturedApi({ isDarkMode }) {
   return (
     <>
       <div className="flex flex-col items-center">
-        <h3 className="text-center pb-8 pt-10 px-20 text-2xl self-center font-bold">About</h3>
+        <h3 className="text-center pb-8 pt-10 px-20 text-2xl self-center font-bold">Featured APIs</h3>
         <div className="flex justify-center items-center flex-col pb-8">
-          {/* Updated the image size and added utility classes for centering */}
-          <img className="w-[40rem] mb-4" src={GlobeImage} alt="" />
-
           <p className="sub-paragraph text-center">
-            Our goal is to create an application that will provide users with personalized local information based on their <strong>geolocation</strong>.
-
-            This platform will offer <strong>real-time weather</strong> updates and relevant <strong>local news</strong> to enhance the user experience and keep them informed about their surroundings.
-
-            It will also include the functionality to enable users to search the weather and news in any location at any given time. As well as the ability to filter news based on their references.
+            Testing
           </p>
         </div>
       </div>
@@ -22,4 +15,4 @@ function FeaturedApi({ isDarkMode }) {
   );
 }
 
-export default About;
+export default FeaturedApi;
