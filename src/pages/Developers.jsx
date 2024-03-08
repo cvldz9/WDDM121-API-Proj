37ae24c46e395bfcd06484bd6179edcc112425b0
Joseph Essel Moses <jmosesessel21@gmail.com>
2024-03-08 00:44:07 -0500

    fix overflow on pages

--- src/pages/Developers.jsx
+++ src/pages/Developers.jsx
@@ -4,57 +4,136 @@ import DanielImage from "../assets/daniel.svg";
 import PattyImage from "../assets/patty.svg";
 import CatherineImage from "../assets/catherine.svg";
 
-
 function Developers({ isDarkMode }) {
-  return (
-    <>
-      <div className="flex flex-col items-center">
-        <h3 className="text-center pb-8 pt-10 px-20 text-2xl self-center font-bold">Developers</h3>
-        <div className="flex justify-center pb-8">
-          <p className="sub-paragraph text-center">Explore this page to learn more about each member of our team. Discover their areas of expertise, passions, and contributions to our projects. Get to know the faces behind the code and see firsthand the dedication and passion that drive us forward. Join us on our journey as we collaborate, innovate, and create groundbreaking solutions together.</p>
-        </div>
+	return (
+		<>
+			<div className="flex flex-col items-center overflow-y-auto">
+				<h3 className="text-center pb-8 pt-10 px-20 text-2xl self-center font-bold">
+					Developers
+				</h3>
+				<div className="flex justify-center pb-8">
+					<p className="sub-paragraph text-center">
+						Explore this page to learn more about each member of our
+						team. Discover their areas of expertise, passions, and
+						contributions to our projects. Get to know the faces
+						behind the code and see firsthand the dedication and
+						passion that drive us forward. Join us on our journey as
+						we collaborate, innovate, and create groundbreaking
+						solutions together.
+					</p>
+				</div>
 
-        <div className="grid gap-10 grid-cols-2 grid-rows-2">
-          <div className="flex box-width rounded-lg shadow-md p-4">
-			    <img className="img-width h-auto" src={JosephImage} alt="" />
-            <div>
-              <p className="m-4 font-semibold text-xl">Joseph Moses</p>
-              <p className="m-4 font-normal text-s text-justify">Joseph is a seasoned web developer with a decade of hands-on experience in the field. Proficient in a wide array of technologies such as <strong>React</strong>, <strong>JavaScript</strong>, and <strong>Vue</strong>, he brings a wealth of knowledge and expertise to any project. With a track record of delivering high-quality solutions, Joseph combines technical prowess with creativity to drive innovation and success.</p>
-			      </div>
-          </div>
+				<div className="grid gap-10 grid-cols-2 grid-rows-2">
+					<div className="flex box-width rounded-lg shadow-md p-4">
+						<img
+							className="img-width h-auto"
+							src={JosephImage}
+							alt=""
+						/>
+						<div>
+							<p className="m-4 font-semibold text-xl">
+								Joseph Moses
+							</p>
+							<p className="m-4 font-normal text-s text-justify">
+								Joseph is a seasoned web developer with a decade
+								of hands-on experience in the field. Proficient
+								in a wide array of technologies such as{" "}
+								<strong>React</strong>,{" "}
+								<strong>JavaScript</strong>, and{" "}
+								<strong>Vue</strong>, he brings a wealth of
+								knowledge and expertise to any project. With a
+								track record of delivering high-quality
+								solutions, Joseph combines technical prowess
+								with creativity to drive innovation and success.
+							</p>
+						</div>
+					</div>
 
-          <div className="flex box-width bg-gray-200 border border-gray-400 rounded-lg shadow-md p-4">
-			      <img className="img-width h-auto" src={DanielImage} alt="" />
-            <div>
-              <p className="m-4 font-semibold text-xl">Daniel Okonkwo</p>
-              <p className="m-4 font-normal text-s text-justify">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam illo quas unde voluptatem perferendis quo quis quidem delectus tempora ad. Molestias inventore dolorem saepe magni deleniti eveniet sunt asperiores aut non aliquid voluptas libero dicta, id sequi sapiente expedita officiis quos nihil ipsum exercitationem. Id molestiae dolore aliquid dolor dicta.</p>
-            </div>
-          </div>
+					<div className="flex box-width bg-gray-200 border border-gray-400 rounded-lg shadow-md p-4">
+						<img
+							className="img-width h-auto"
+							src={DanielImage}
+							alt=""
+						/>
+						<div>
+							<p className="m-4 font-semibold text-xl">
+								Daniel Okonkwo
+							</p>
+							<p className="m-4 font-normal text-s text-justify">
+								Lorem, ipsum dolor sit amet consectetur
+								adipisicing elit. Quam illo quas unde voluptatem
+								perferendis quo quis quidem delectus tempora ad.
+								Molestias inventore dolorem saepe magni deleniti
+								eveniet sunt asperiores aut non aliquid voluptas
+								libero dicta, id sequi sapiente expedita
+								officiis quos nihil ipsum exercitationem. Id
+								molestiae dolore aliquid dolor dicta.
+							</p>
+						</div>
+					</div>
 
-          <div className="flex box-width bg-gray-200 border border-gray-400 rounded-lg shadow-md p-4">
-            <img className="img-width h-auto" src={PattyImage} alt="" />
-            <div>
-              <p className="m-4 font-semibold text-xl">Patty Bactad</p>
-              <p className="m-4 font-normal text-s text-justify">Patty is a seasoned <strong>Senior Information Security Engineer</strong> with 8 years of experience. She excels in developing robust security protocols and conducting vulnerability assessments. Additionally, Patty brings proficiency in <strong>graphic</strong> and <strong>web design</strong>, combining creativity with her technical expertise to deliver innovative solutions and captivating design projects.</p>
-            </div>
-          </div>
+					<div className="flex box-width bg-gray-200 border border-gray-400 rounded-lg shadow-md p-4">
+						<img
+							className="img-width h-auto"
+							src={PattyImage}
+							alt=""
+						/>
+						<div>
+							<p className="m-4 font-semibold text-xl">
+								Patty Bactad
+							</p>
+							<p className="m-4 font-normal text-s text-justify">
+								Patty is a seasoned{" "}
+								<strong>
+									Senior Information Security Engineer
+								</strong>{" "}
+								with 8 years of experience. She excels in
+								developing robust security protocols and
+								conducting vulnerability assessments.
+								Additionally, Patty brings proficiency in{" "}
+								<strong>graphic</strong> and{" "}
+								<strong>web design</strong>, combining
+								creativity with her technical expertise to
+								deliver innovative solutions and captivating
+								design projects.
+							</p>
+						</div>
+					</div>
 
-          <div className="flex box-width bg-gray-200 border border-gray-400 rounded-lg shadow-md p-4">
-            <img className="img-width h-auto" src={CatherineImage} alt="" />
-            <div>
-              <p className="m-4 font-semibold text-xl">Catherine Valdez</p>
-              <p className="m-4 font-normal text-s text-justify">Catherine has a strong background in <strong>software testing</strong>, <strong>UI/UX design</strong>, and <strong>web design</strong>. With her expertise, she ensures the quality and functionality of software products through rigorous testing methodologies. Additionally, her keen eye for design and user experience allows her to create visually appealing and intuitive interfaces that enhance user engagement and satisfaction.</p>
-            </div>
-          </div>
+					<div className="flex box-width bg-gray-200 border border-gray-400 rounded-lg shadow-md p-4">
+						<img
+							className="img-width h-auto"
+							src={CatherineImage}
+							alt=""
+						/>
+						<div>
+							<p className="m-4 font-semibold text-xl">
+								Catherine Valdez
+							</p>
+							<p className="m-4 font-normal text-s text-justify">
+								Catherine has a strong background in{" "}
+								<strong>software testing</strong>,{" "}
+								<strong>UI/UX design</strong>, and{" "}
+								<strong>web design</strong>. With her expertise,
+								she ensures the quality and functionality of
+								software products through rigorous testing
+								methodologies. Additionally, her keen eye for
+								design and user experience allows her to create
+								visually appealing and intuitive interfaces that
+								enhance user engagement and satisfaction.
+							</p>
+						</div>
+					</div>
 
-          <div>
-            <a href="https://www.cartoonize.net/avatar-maker/">Resource: ColorCinch</a>
-          </div>
-
-        </div>
-      </div>
-    </>
-  );
+					<div>
+						<a href="https://www.cartoonize.net/avatar-maker/">
+							Resource: ColorCinch
+						</a>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default Developers;
--- src/pages/FeaturedApi.jsx
+++ src/pages/FeaturedApi.jsx
@@ -1,33 +1,47 @@
 import "./FeaturedApi.css";
 
 function FeaturedApi({ isDarkMode }) {
-  return (
-    <div className="bg-white min-h-screen flex flex-col">
-      <div className="container mx-auto px-6 lg:px-8">
-        <h2 className="py-8 text-3xl font-bold">Featured APIs</h2>
-        <div className="flex flex-wrap justify-center -mx-4">
-          <div className="mx-4 my-2 flex flex-col max-w-xs">
-            <dt className="text-base leading-7 text-gray-600">
-              This API will be used to allow the user to input their specific location, to feed the other APIs integrated in this API
-            </dt>
-            <dd className="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">Geolocation</dd>
-          </div>
-          <div className="mx-4 my-2 flex flex-col max-w-xs">
-            <dt className="text-base leading-7 text-gray-600">
-              This API will be used to get the current weather information of the user based on the current location (longitude and latitude) of the user from the Geolocation API
-            </dt>
-            <dd className="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">Weather</dd>
-          </div>
-          <div className="mx-4 my-2 flex flex-col max-w-xs">
-            <dt className="text-base leading-7 text-gray-600">
-              This API will then take the “name” of the city from the response of the Weather API and use it to get the weather news for the current or preferred location of the user
-            </dt>
-            <dd className="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">News</dd>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="bg-white min-h-screen flex flex-col overflow-y-auto">
+			<div className="container mx-auto px-6 lg:px-8">
+				<h2 className="py-8 text-3xl font-bold">Featured APIs</h2>
+				<div className="flex flex-wrap justify-center -mx-4">
+					<div className="mx-4 my-2 flex flex-col max-w-xs">
+						<dt className="text-base leading-7 text-gray-600">
+							This API will be used to allow the user to input
+							their specific location, to feed the other APIs
+							integrated in this API
+						</dt>
+						<dd className="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
+							Geolocation
+						</dd>
+					</div>
+					<div className="mx-4 my-2 flex flex-col max-w-xs">
+						<dt className="text-base leading-7 text-gray-600">
+							This API will be used to get the current weather
+							information of the user based on the current
+							location (longitude and latitude) of the user from
+							the Geolocation API
+						</dt>
+						<dd className="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
+							Weather
+						</dd>
+					</div>
+					<div className="mx-4 my-2 flex flex-col max-w-xs">
+						<dt className="text-base leading-7 text-gray-600">
+							This API will then take the “name” of the city from
+							the response of the Weather API and use it to get
+							the weather news for the current or preferred
+							location of the user
+						</dt>
+						<dd className="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
+							News
+						</dd>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default FeaturedApi;
