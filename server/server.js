9a23752ffa26cd99c3e60bdafac406f4b0c84676
Joseph Essel Moses <jmosesessel21@gmail.com>
2024-04-03 00:39:51 -0400

    configured the proxy and concurrency

--- server/server.js
+++ server/server.js
@@ -3,6 +3,14 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+
+// Define routes
+app.get('/login', (req, res) => {
+    res.send({ title: 'Welcome to Upload!' });
+  });
+  
+
+
 app.listen(PORT, () => {
 	console.log(`Server is listening on port ${PORT}`);
 });
